d3ac291aac4bf233b5dc887c51413c89a9780f6b
Pham Dang Khoa <khoa199960@gmail.com>
2023-06-30 10:19:03 +0700

    Update ObjectPlatformRotate.cs

--- QuickMethode/Assets/Project-QuickMethode/Rigidbody/Object/ObjectPlatformRotate.cs
+++ QuickMethode/Assets/Project-QuickMethode/Rigidbody/Object/ObjectPlatformRotate.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class ObjectPlatformRotate : MonoBehaviour
 {
-    #region Varible: Circle
+    [SerializeField] private DirectionX m_dir = DirectionX.Left;
+    [SerializeField] private float m_speed = 10f;
 
-    [SerializeField] private DirectionX m_dir = DirectionX.None;
-    [SerializeField] private float m_speed = 50f;
-    [SerializeField] private float m_radius = 5f;
+    [SerializeField] private CircleCollider2D m_circleCollider;
+    [SerializeField] private float m_radius = 1f;
 
+    public DirectionX Dir { get => m_dir; set => m_dir = value; }
+    public float Speed { get => m_speed; set => m_speed = value; }
+    public float Radius
+    {
+        get => m_circleCollider != null ? m_circleCollider.radius * transform.localScale.x : m_radius;
+        set
+        {
+            if (m_circleCollider != null)
+                m_circleCollider.radius = value;
+            else
+                m_radius = value;
+        }
+    }
+    public Vector2 Centre => m_circleCollider != null ? m_circleCollider.bounds.center : this.transform.position;
     public float SurfaceForce => m_speed * m_radius * (int)m_dir * Time.fixedDeltaTime;
 
-    #endregion
-
-    private Rigidbody2D m_rigidbody;
+    [HideInInspector] private Rigidbody2D m_rigidbody;
 
     private void Start()
     {
@@ -26,5 +38,10 @@ public class ObjectPlatformRotate : MonoBehaviour
         m_rigidbody.angularVelocity = m_speed * (int)m_dir * -1;
         if (m_rigidbody.rotation >= 360f || m_rigidbody.rotation <= -360f)
             m_rigidbody.rotation = 0;
+    }
+
+    private void OnDrawGizmos()
+    {
+        QGizmos.SetLine(this.transform.position, this.transform.position + QCircle.GetPosXY(transform.eulerAngles.z, Radius), Color.red);
     }
 }
