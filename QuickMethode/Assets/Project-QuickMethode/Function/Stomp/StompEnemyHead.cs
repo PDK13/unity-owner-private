bdaed1190ad1bc2807a641c154df1bcb77b0c965
Pham Dang Khoa <khoa199960@gmail.com>
2023-07-20 11:25:05 +0700

    Update Stomp Player and Enemy

---
+++ QuickMethode/Assets/Project-QuickMethode/Function/Stomp/StompEnemyHead.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class StompEnemyHead : MonoBehaviour
+{
+    //NOTE: Should use 2 layer for Enemy Head and Player Foot that only contact each other!!
+
+    [SerializeField] private GameObject m_enemy;
+    [SerializeField] private string m_methode;
+
+    private void OnStompReceive(string EnemyReceive)
+    {
+        if (m_enemy != null)
+            m_enemy.SendMessage(m_methode, EnemyReceive, SendMessageOptions.DontRequireReceiver);
+    }
+}
---
+++ QuickMethode/Assets/Project-QuickMethode/Function/Stomp/StompPlayerFoot.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class StompPlayerFoot : MonoBehaviour
+{
+    //NOTE: Should use 2 layer for Enemy Head and Player Foot that only contact each other!!
+
+    [SerializeField] private GameObject m_player;
+    [SerializeField] private string m_methode;
+
+    [Space]
+    [Tooltip("Send an message to 'StompEnemyHead'")]
+    [SerializeField] private string m_enemyMessage = "";
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (m_player != null)
+            m_player.SendMessage(m_methode, SendMessageOptions.DontRequireReceiver);
+        //
+        collision.gameObject.SendMessage("OnStompReceive", m_enemyMessage, SendMessageOptions.DontRequireReceiver);
+    }
+}
