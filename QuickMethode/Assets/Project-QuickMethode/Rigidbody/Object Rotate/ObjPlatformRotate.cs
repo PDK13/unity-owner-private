1210a2ce2087764abb87a8cbee20d5cd018cffe1
Pham Dang Khoa <khoa199960@gmail.com>
2023-07-23 14:45:50 +0700

    Update Object Platform Circle Editor

--- QuickMethode/Assets/Project-QuickMethode/Rigidbody/Object Rotate/ObjPlatformRotate.cs
+++ QuickMethode/Assets/Project-QuickMethode/Rigidbody/Object Rotate/ObjPlatformRotate.cs
@@ -2,6 +2,9 @@ using QuickMethode;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class ObjPlatformRotate : MonoBehaviour
 {
@@ -48,3 +51,41 @@ public class ObjPlatformRotate : MonoBehaviour
         QGizmos.SetLine(this.transform.position, this.transform.position + QCircle.GetPosXY(transform.eulerAngles.z, Radius), Color.red);
     }
 }
+
+#if UNITY_EDITOR
+
+[CustomEditor(typeof(ObjPlatformRotate))]
+public class ObjPlatformRotateEditor : Editor
+{
+    ObjPlatformRotate m_target;
+
+    private SerializedProperty m_dir;
+    private SerializedProperty m_speed;
+    private SerializedProperty m_circleCollider;
+    private SerializedProperty m_radius;
+
+    private void OnEnable()
+    {
+        m_target = (target as ObjPlatformRotate);
+
+        m_dir = serializedObject.FindProperty("m_dir");
+        m_speed = serializedObject.FindProperty("m_speed");
+        m_circleCollider = serializedObject.FindProperty("m_circleCollider");
+        m_radius = serializedObject.FindProperty("m_radius");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(m_dir);
+        EditorGUILayout.PropertyField(m_speed);
+        QEditorCustom.SetChanceCheckBegin();
+        EditorGUILayout.PropertyField(m_circleCollider);
+        if (QEditorCustom.SetChanceCheckEnd())
+            if (!m_target.CircleCollider)
+                EditorGUILayout.PropertyField(m_radius);
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+
+#endif
