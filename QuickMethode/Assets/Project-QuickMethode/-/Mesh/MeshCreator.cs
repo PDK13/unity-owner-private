6d7023f54e1d520f462c103a6bc50a2b15506ba7
Pham Dang Khoa <khoa199960@gmail.com>
2023-08-31 16:23:10 +0700

    Update MeshCreator.cs

--- QuickMethode/Assets/Project-QuickMethode/-/Mesh/MeshCreator.cs
+++ QuickMethode/Assets/Project-QuickMethode/-/Mesh/MeshCreator.cs
@@ -8,12 +8,22 @@ public class MeshCreator : MonoBehaviour
 {
     public List<Vector2> Points;
 
+    [Min(3)] public int GeometryPoints = 3;
+    [Min(0)] public float GeometryRadius = 2f;
+    [Range(0, 360)] public float GeometryRotate = 0f;
+
     public void SetCreate()
     {
         if (GetComponent<MeshFilter>() == null)
             return;
         //
         GetComponent<MeshFilter>().mesh = QMesh.GetMesh(Points);
+    }
+
+    public void SetCreateGeometry()
+    {
+        Points = QGeometry.GetGeometry(GeometryPoints, GeometryRadius, GeometryRotate);
+        SetCreate();
     }
 }
 
@@ -26,11 +36,19 @@ public class MeshCreatorEditor : Editor
 
     private SerializedProperty Points;
 
+    private SerializedProperty GeometryPoints;
+    private SerializedProperty GeometryRadius;
+    private SerializedProperty GeometryRotate;
+
     private void OnEnable()
     {
         m_target = target as MeshCreator;
         //
         Points = QEditorCustom.GetField(this, "Points");
+        //
+        GeometryPoints = QEditorCustom.GetField(this, "GeometryPoints");
+        GeometryRadius = QEditorCustom.GetField(this, "GeometryRadius");
+        GeometryRotate = QEditorCustom.GetField(this, "GeometryRotate");
     }
 
     public override void OnInspectorGUI()
@@ -41,6 +59,13 @@ public class MeshCreatorEditor : Editor
         //
         if (QEditor.SetButton("Generate"))
             m_target.SetCreate();
+        //
+        QEditorCustom.SetField(GeometryPoints);
+        QEditorCustom.SetField(GeometryRadius);
+        QEditorCustom.SetField(GeometryRotate);
+        //
+        if (QEditor.SetButton("Generate Geometry"))
+            m_target.SetCreateGeometry();
         //
         QEditorCustom.SetApply(this);
     }
