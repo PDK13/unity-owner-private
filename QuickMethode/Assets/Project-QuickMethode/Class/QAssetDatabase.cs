e98557a4a1a157dbf467ff449a29609302ee0c13
Khoa Pham Dang <khoa199960@gmail.com>
2023-10-08 22:26:18 +0700

    Update QAssetDatabase.cs

--- QuickMethode/Assets/Project-QuickMethode/Class/QAssetDatabase.cs
+++ QuickMethode/Assets/Project-QuickMethode/Class/QAssetDatabase.cs
@@ -1,5 +1,8 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.Audio;
+using UnityEngine.Video;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -13,60 +16,54 @@ public class QAssetsDatabase : QPath
 {
     //Folder "Assets" is the main root of all assets in project, that can find any assets from it.
 
-    public const string ExamplePathAssets = "Assets/Scene";
-
     #region ==================================== Create Folder
 
     public static string SetCreateFolder(params string[] PathChildInAssets)
     {
         List<string> Path = new List<string>();
-
+        //
         string PathString = "";
-
+        //
         for (int i = 0; i < PathChildInAssets.Length; i++)
         {
             Path.Add(PathChildInAssets[i]);
-
+            //
             QEncypt.GetEncyptAdd('/', PathString, PathChildInAssets[i], out PathString);
-
+            //
             SetCreateFolderExist(Path.ToArray());
         }
-
+        //
         return PathString;
     }
 
     private static string SetCreateFolderExist(params string[] PathChildInAssets)
     {
         //If Root Folder not Exist, then can't Create new Folder from that Root Folder
-
+        //
         string PathInAssets = "Assets";
-
+        //
         for (int i = 0; i < PathChildInAssets.Length - 1; i++)
-        {
             QEncypt.GetEncyptAdd('/', PathInAssets, PathChildInAssets[i], out PathInAssets);
-        }
-
+        //
         string PathFolderInAssets = PathChildInAssets[PathChildInAssets.Length - 1];
 
         if (QPath.GetPathFolderExist(PathType.Assets, PathChildInAssets))
         {
             //Debug.LogWarningFormat("[Debug] Folder Exist!!\n{0}", PathInAssets + "/" + PathFolderInAssets);
-
             return "";
         }
-
+        //
         try
         {
             string PathString = AssetDatabase.CreateFolder(PathInAssets, PathFolderInAssets);
-
+            //
             SetRefresh();
-
+            //
             return AssetDatabase.GUIDToAssetPath(PathString);
         }
         catch
         {
             //Debug.LogWarningFormat("[Debug] Root Folder not Exist!!\n{0}", PathInAssets + "/" + PathFolderInAssets);
-
             return "";
         }
     }
@@ -79,7 +76,7 @@ public class QAssetsDatabase : QPath
     {
         FileUtil.DeleteFileOrDirectory(QPath.GetPath(PathType, PathChild) + ".meta");
         FileUtil.DeleteFileOrDirectory(QPath.GetPath(PathType, PathChild));
-
+        //
         SetRefresh();
     }
 
@@ -96,16 +93,195 @@ public class QAssetsDatabase : QPath
 
     #region ==================================== Get
 
-    public static List<GameObject> GetPrefab(string NameContains, params string[] PathChildInAssets)
+    public static List<AnimationClip> GetAnimationClip(string NameSpecial, string PathAssets = "Assets/")
     {
-        string Path = QPath.GetPath(PathType.Assets, PathChildInAssets);
+        if (PathAssets == null)
+            PathAssets = "Assets/";
+        else
+        if (PathAssets == "")
+            PathAssets = "Assets/";
+        else
+        if (!GetPathFolderExist(PathAssets))
+            return null;
         //
-        if (!GetPathFolderExist(Path))
+        List<AnimationClip> ObjectsFound = new List<AnimationClip>();
+        //
+        string[] GUIDPathUnityFound = AssetDatabase.FindAssets(string.Format("{0} {1}", NameSpecial, "t:AnimationClip"), new string[] { PathAssets });
+        //
+        foreach (string GUIDPath in GUIDPathUnityFound)
+        {
+            string AssetsSinglePath = AssetDatabase.GUIDToAssetPath(GUIDPath);
+            AnimationClip ObjectFound = AssetDatabase.LoadAssetAtPath<AnimationClip>(AssetsSinglePath);
+            ObjectsFound.Add(ObjectFound);
+        }
+        //
+        return ObjectsFound;
+    }
+
+    public static List<AudioClip> GetAudioClip(string NameSpecial, string PathAssets = "Assets/")
+    {
+        if (PathAssets == null)
+            PathAssets = "Assets/";
+        else
+        if (PathAssets == "")
+            PathAssets = "Assets/";
+        else
+        if (!GetPathFolderExist(PathAssets))
+            return null;
+        //
+        List<AudioClip> ObjectsFound = new List<AudioClip>();
+        //
+        string[] GUIDPathUnityFound = AssetDatabase.FindAssets(string.Format("{0} {1}", NameSpecial, "t:AudioClip"), new string[] { PathAssets });
+        //
+        foreach (string GUIDPath in GUIDPathUnityFound)
+        {
+            string AssetsSinglePath = AssetDatabase.GUIDToAssetPath(GUIDPath);
+            AudioClip ObjectFound = AssetDatabase.LoadAssetAtPath<AudioClip>(AssetsSinglePath);
+            ObjectsFound.Add(ObjectFound);
+        }
+        //
+        return ObjectsFound;
+    }
+
+    public static List<Font> GetFont(string NameSpecial, string PathAssets = "Assets/")
+    {
+        if (PathAssets == null)
+            PathAssets = "Assets/";
+        else
+        if (PathAssets == "")
+            PathAssets = "Assets/";
+        else
+        if (!GetPathFolderExist(PathAssets))
+            return null;
+        //
+        List<Font> ObjectsFound = new List<Font>();
+        //
+        string[] GUIDPathUnityFound = AssetDatabase.FindAssets(string.Format("{0} {1}", NameSpecial, "t:Font"), new string[] { PathAssets });
+        //
+        foreach (string GUIDPath in GUIDPathUnityFound)
+        {
+            string AssetsSinglePath = AssetDatabase.GUIDToAssetPath(GUIDPath);
+            Font ObjectFound = AssetDatabase.LoadAssetAtPath<Font>(AssetsSinglePath);
+            ObjectsFound.Add(ObjectFound);
+        }
+        //
+        return ObjectsFound;
+    }
+
+    public static List<Material> GetMaterial(string NameSpecial, string PathAssets = "Assets/")
+    {
+        if (PathAssets == null)
+            PathAssets = "Assets/";
+        else
+        if (PathAssets == "")
+            PathAssets = "Assets/";
+        else
+        if (!GetPathFolderExist(PathAssets))
+            return null;
+        //
+        List<Material> ObjectsFound = new List<Material>();
+        //
+        string[] GUIDPathUnityFound = AssetDatabase.FindAssets(string.Format("{0} {1}", NameSpecial, "t:Material"), new string[] { PathAssets });
+        //
+        foreach (string GUIDPath in GUIDPathUnityFound)
+        {
+            string AssetsSinglePath = AssetDatabase.GUIDToAssetPath(GUIDPath);
+            Material ObjectFound = AssetDatabase.LoadAssetAtPath<Material>(AssetsSinglePath);
+            ObjectsFound.Add(ObjectFound);
+        }
+        //
+        return ObjectsFound;
+    }
+
+    public static List<PhysicMaterial> GetPhysicMaterial(string NameSpecial, string PathAssets = "Assets/")
+    {
+        if (PathAssets == null)
+            PathAssets = "Assets/";
+        else
+        if (PathAssets == "")
+            PathAssets = "Assets/";
+        else
+        if (!GetPathFolderExist(PathAssets))
+            return null;
+        //
+        List<PhysicMaterial> ObjectsFound = new List<PhysicMaterial>();
+        //
+        string[] GUIDPathUnityFound = AssetDatabase.FindAssets(string.Format("{0} {1}", NameSpecial, "t:PhysicMaterial"), new string[] { PathAssets });
+        //
+        foreach (string GUIDPath in GUIDPathUnityFound)
+        {
+            string AssetsSinglePath = AssetDatabase.GUIDToAssetPath(GUIDPath);
+            PhysicMaterial ObjectFound = AssetDatabase.LoadAssetAtPath<PhysicMaterial>(AssetsSinglePath);
+            ObjectsFound.Add(ObjectFound);
+        }
+        //
+        return ObjectsFound;
+    }
+
+    public static List<Texture> GetTexture(string NameSpecial, string PathAssets = "Assets/")
+    {
+        if (PathAssets == null)
+            PathAssets = "Assets/";
+        else
+        if (PathAssets == "")
+            PathAssets = "Assets/";
+        else
+        if (!GetPathFolderExist(PathAssets))
+            return null;
+        //
+        List<Texture> ObjectsFound = new List<Texture>();
+        //
+        string[] GUIDPathUnityFound = AssetDatabase.FindAssets(string.Format("{0} {1}", NameSpecial, "t:Texture"), new string[] { PathAssets });
+        //
+        foreach (string GUIDPath in GUIDPathUnityFound)
+        {
+            string AssetsSinglePath = AssetDatabase.GUIDToAssetPath(GUIDPath);
+            Texture ObjectFound = AssetDatabase.LoadAssetAtPath<Texture>(AssetsSinglePath);
+            ObjectsFound.Add(ObjectFound);
+        }
+        //
+        return ObjectsFound;
+    }
+
+    public static List<VideoClip> GetVideoClip(string NameSpecial, string PathAssets = "Assets/")
+    {
+        if (PathAssets == null)
+            PathAssets = "Assets/";
+        else
+        if (PathAssets == "")
+            PathAssets = "Assets/";
+        else
+        if (!GetPathFolderExist(PathAssets))
+            return null;
+        //
+        List<VideoClip> ObjectsFound = new List<VideoClip>();
+        //
+        string[] GUIDPathUnityFound = AssetDatabase.FindAssets(string.Format("{0} {1}", NameSpecial, "t:VideoClip"), new string[] { PathAssets });
+        //
+        foreach (string GUIDPath in GUIDPathUnityFound)
+        {
+            string AssetsSinglePath = AssetDatabase.GUIDToAssetPath(GUIDPath);
+            VideoClip ObjectFound = AssetDatabase.LoadAssetAtPath<VideoClip>(AssetsSinglePath);
+            ObjectsFound.Add(ObjectFound);
+        }
+        //
+        return ObjectsFound;
+    }
+
+    public static List<GameObject> GetPrefab(string NameSpecial, string PathAssets = "Assets/")
+    {
+        if (PathAssets == null)
+            PathAssets = "Assets/";
+        else
+        if (PathAssets == "")
+            PathAssets = "Assets/";
+        else
+        if (!GetPathFolderExist(PathAssets))
             return null;
         //
         List<GameObject> ObjectsFound = new List<GameObject>();
         //
-        string[] GUIDPathUnityFound = AssetDatabase.FindAssets(string.Format("{0} {1}", NameContains, "t:prefab"), new string[] { Path });
+        string[] GUIDPathUnityFound = AssetDatabase.FindAssets(string.Format("{0} {1}", NameSpecial, "t:Prefab"), new string[] { PathAssets });
         //
         foreach (string GUIDPath in GUIDPathUnityFound)
         {
@@ -117,16 +293,20 @@ public class QAssetsDatabase : QPath
         return ObjectsFound;
     }
 
-    public static List<Sprite> GetSprite(string NameContains, params string[] PathChildInAssets)
+    public static List<Sprite> GetSprite(string NameSpecial, string PathAssets = "Assets/")
     {
-        string Path = QPath.GetPath(PathType.Assets, PathChildInAssets);
-        //
-        if (!GetPathFolderExist(Path))
+        if (PathAssets == null)
+            PathAssets = "Assets/";
+        else
+        if (PathAssets == "")
+            PathAssets = "Assets/";
+        else
+        if (!GetPathFolderExist(PathAssets))
             return null;
         //
         List<Sprite> ObjectsFound = new List<Sprite>();
         //
-        string[] GUIDPathUnityFound = AssetDatabase.FindAssets(string.Format("{0} {1}", NameContains, "t:sprite"), new string[] { Path });
+        string[] GUIDPathUnityFound = AssetDatabase.FindAssets(string.Format("{0} {1}", NameSpecial, "t:Sprite"), new string[] { PathAssets });
         //
         foreach (string GUIDPath in GUIDPathUnityFound)
         {
@@ -138,29 +318,26 @@ public class QAssetsDatabase : QPath
         return ObjectsFound;
     }
 
-    public static List<RuntimeAnimatorController> GetAnimatorController(string NameContains, params string[] PathChildInAssets)
+    public static List<RuntimeAnimatorController> GetAnimatorController(string NameSpecial, string PathAssets = "Assets/")
     {
-        string Path = QPath.GetPath(PathType.Assets, PathChildInAssets);
-        //
-        if (!GetPathFolderExist(Path))
+        if (PathAssets == null)
+            PathAssets = "Assets/";
+        else
+        if (PathAssets == "")
+            PathAssets = "Assets/";
+        else
+        if (!GetPathFolderExist(PathAssets))
             return null;
         //
         List<RuntimeAnimatorController> ObjectsFound = new List<RuntimeAnimatorController>();
         //
-        if (NameContains == null)
-            NameContains = "*";
-        else
-        if (NameContains == "")
-            NameContains = "*";
-        //
-        string[] GUIDPathUnityFound = AssetDatabase.FindAssets(NameContains);
+        string[] GUIDPathUnityFound = AssetDatabase.FindAssets(string.Format("{0} {1}", NameSpecial, "t:AnimatorController"), new string[] { PathAssets });
         //
         foreach (string GUIDPath in GUIDPathUnityFound)
         {
             string AssetsSinglePath = AssetDatabase.GUIDToAssetPath(GUIDPath);
-            RuntimeAnimatorController ObjectFound = (RuntimeAnimatorController)AssetDatabase.LoadAssetAtPath(AssetsSinglePath, typeof(RuntimeAnimatorController));
-            if (ObjectFound != null)
-                ObjectsFound.Add(ObjectFound);
+            RuntimeAnimatorController ObjectFound = AssetDatabase.LoadAssetAtPath<RuntimeAnimatorController>(AssetsSinglePath);
+            ObjectsFound.Add(ObjectFound);
         }
         //
         return ObjectsFound;
