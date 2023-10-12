321e61ab04a584011c6e41fe7cf0d9e32d6e2792
Pham Dang Khoa <khoa199960@gmail.com>
2023-10-12 17:38:06 +0700

    update

---
+++ QuickMethode/Assets/Project-QuickMethode/Renderer/RendererGeometry/ShapePlatformCreator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class ShapePlatformCreator : MonoBehaviour
+{
+    [SerializeField] private SpriteShapeController m_spriteShape;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+
+}
