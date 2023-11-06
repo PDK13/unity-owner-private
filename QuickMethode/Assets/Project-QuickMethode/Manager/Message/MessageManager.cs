0846efa1808f47a15b9e5a3aa36d90e1ec593ef5
Pham Dang Khoa <khoa199960@gmail.com>
2023-11-06 17:29:36 +0700

    update message manager

---
+++ QuickMethode/Assets/Project-QuickMethode/Manager/Message/MessageManager.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class MessageManager : MonoBehaviour
+{
+    public static MessageManager Instance;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        //
+        Instance = this;
+    }
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+
+    public IEnumerator ISetWrite(TextMeshProUGUI TextMessPro, MessageConfig MessageData)
+    {
+        foreach (MessageConfigSingle MessageSingle in MessageData.List)
+        {
+            bool ColorFormat = false;
+            //
+            string Text = MessageSingle.Text;
+            //
+            if (Text != null)
+            {
+                if (Text != "")
+                {
+                    foreach (char MessageChar in Text)
+                    {
+                        //TEXT:
+                        TextMessPro.text += MessageChar;
+                        //
+                        //COLOR:
+                        if (!ColorFormat && MessageChar == '<')
+                        {
+                            ColorFormat = true;
+                            continue;
+                        }
+                        else
+                        if (ColorFormat && MessageChar == '>')
+                        {
+                            ColorFormat = false;
+                            continue;
+                        }
+                        //
+                        //DELAY:
+                        if (ColorFormat)
+                            continue;
+                        //
+                        if (MessageSingle.DelaySpace > 0 && MessageChar == ' ')
+                            yield return new WaitForSeconds(MessageSingle.DelaySpace);
+                        else
+                        if (MessageSingle.DelayAlpha > 0)
+                            yield return new WaitForSeconds(MessageSingle.DelayAlpha);
+                    }
+                }
+            }
+            //
+            if (MessageSingle.DelayFinal > 0)
+                yield return new WaitForSeconds(MessageSingle.DelayFinal);
+        }
+    }
+}
--- QuickMethode/Assets/Project-QuickMethode/NewBehaviourScript.cs
+++ QuickMethode/Assets/Project-QuickMethode/NewBehaviourScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    [SerializeField][TextArea] private string m_text = "Hello World!!";
+    [SerializeField] private MessageConfig m_messageConfig;
     [SerializeField] private TextMeshProUGUI m_tmpMyText;
 
     private IEnumerator Start()
@@ -14,14 +14,16 @@ public class NewBehaviourScript : MonoBehaviour
         //
         yield return new WaitForSeconds(3f);
         //
-        MessageConfig MessageData = new MessageConfig();
-        MessageData.SetWriteByCharacter("Hello there!\n", 0.1f);
-        MessageData.SetDelay(2f);
-        MessageData.SetWriteByWord("I WILL KILL YOU!\n", 1f);
-        MessageData.SetDelay(2f);
-        MessageData.SetWrite("HA HA HA!\n", Color.red, 0.1f, 1f);
+        //MessageConfig MessageData = new MessageConfig();
+        //MessageData.SetWriteByCharacter("Hello there!\n", 0.1f);
+        //MessageData.SetDelay(2f);
+        //MessageData.SetWriteByWord("I WILL KILL YOU!\n", 1f);
+        //MessageData.SetDelay(2f);
+        //MessageData.SetWrite("HA HA HA!\n", Color.red, 0.1f, 1f);
         //
-        yield return MessageManager.Instance.ISetWrite(m_tmpMyText, MessageData);
+        //yield return MessageManager.Instance.ISetWrite(m_tmpMyText, MessageData);
+        //
+        yield return MessageManager.Instance.ISetWrite(m_tmpMyText, m_messageConfig);
         //
         Debug.Log("[Debug] Message End!!");
     }
