318960f1145915582784c322d23ed9802be47fca
Pham Dang Khoa <khoa199960@gmail.com>
2023-11-14 17:40:02 +0700

    update water sort

---
+++ QuickMethode/Assets/Project-QuickMethode/-/WaterSort/script/BottleController.cs
@@ -0,0 +1,197 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BottleController : MonoBehaviour
+{
+    private const int COLOR_MAX = 4;
+
+    [SerializeField] private List<Color> m_bottleColor = new List<Color>();
+
+    private Color BottleColorTop => m_bottleColor.Count > 0 ? m_bottleColor[m_bottleColor.Count - 1] : Color.clear;
+
+    [Space]
+    [SerializeField] private float m_timeRotate = 3.0f;
+    private float m_timeRotateCurrent;
+    private float m_timeRotateLerp;
+
+    [Space]
+    [SerializeField] private AnimationCurve m_curveFillAmount;
+    [SerializeField] private AnimationCurve m_curveScaleAndRotation;
+    [SerializeField] private List<float> m_limitRotation = new List<float>();
+    [SerializeField] private List<float> m_limitFillAmount = new List<float>();
+
+    private int LimitRotationIndex
+    {
+        get
+        {
+            Color ColorTop = BottleColorTop;
+            for (int i = m_bottleColor.Count - 1; i >= 0; i--)
+                if (!m_bottleColor[i].Equals(ColorTop))
+                    return i + 1;
+            return 0;
+        }
+    }
+
+    private float LimitRotation => m_limitRotation[LimitRotationIndex];
+
+    private float m_limitRotationValue;
+
+    private bool m_rotateActive = false;
+
+    [Space]
+    [SerializeField] private SpriteRenderer m_bottleMark;
+
+    private Material m_bottleMaterial => !Application.isPlaying ? m_bottleMark.sharedMaterial : m_bottleMark.material;
+
+    private void Start()
+    {
+        m_bottleMaterial.SetFloat("_FillAmount", m_limitFillAmount[m_bottleColor.Count]);
+        //
+        SetUpdateColorStart();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            SetColorOut();
+    }
+
+    private void SetUpdateColorStart()
+    {
+        for (int i = 0; i < m_bottleColor.Count; i++)
+            m_bottleMaterial.SetColor(string.Format("_Color{0}", i), m_bottleColor[i]);
+    }
+
+    private void SetUpdateColorController()
+    {
+
+    }
+
+    public void SetColorOut()
+    {
+        if (m_rotateActive)
+            return;
+        //
+        if (m_bottleColor.Count == 0)
+            return;
+        //
+        StartCoroutine(ISetRotate());
+    }
+
+    private IEnumerator ISetRotate()
+    {
+        m_rotateActive = true;
+        //
+        m_timeRotateCurrent = 0; //Time in curve to get value at the time of lerp value!
+        m_timeRotateLerp = 0; //Time in ratio of Time Rotate from 0..1 value!
+        float AngleValue = 0;
+        m_limitRotationValue = LimitRotation;
+        //
+        while (m_timeRotateCurrent < m_timeRotate)
+        {
+            m_timeRotateLerp = m_timeRotateCurrent / m_timeRotate;
+            AngleValue = Mathf.Lerp(0.0f, m_limitRotationValue, m_timeRotateLerp);
+            transform.eulerAngles = Vector3.forward * AngleValue;
+            //
+            if (m_limitFillAmount[m_bottleColor.Count] > m_curveFillAmount.Evaluate(AngleValue))
+                m_bottleMaterial.SetFloat("_FillAmount", m_curveFillAmount.Evaluate(AngleValue));
+            m_bottleMaterial.SetFloat("_ScaleAndRotate", m_curveScaleAndRotation.Evaluate(AngleValue));
+            //
+            m_timeRotateCurrent += Time.deltaTime;
+            //
+            yield return new WaitForEndOfFrame();
+        }
+        //
+        AngleValue = m_limitRotationValue;
+        transform.eulerAngles = Vector3.forward * AngleValue;
+        m_bottleMaterial.SetFloat("_FillAmount", m_curveFillAmount.Evaluate(AngleValue));
+        m_bottleMaterial.SetFloat("_ScaleAndRotate", m_curveScaleAndRotation.Evaluate(AngleValue));
+        //
+        yield return ISetRotateBack();
+        //
+        m_bottleColor.RemoveAt(m_bottleColor.Count - 1);
+        //
+        m_rotateActive = false;
+    }
+
+    private IEnumerator ISetRotateBack()
+    {
+        m_timeRotateCurrent = 0; //Time in curve to get value at the time!
+        m_timeRotateLerp = 0;
+        float AngleValue = 0;
+        //
+        while (m_timeRotateCurrent < m_timeRotate)
+        {
+            m_timeRotateLerp = m_timeRotateCurrent / m_timeRotate;
+            AngleValue = Mathf.Lerp(m_limitRotationValue, 0.00f, m_timeRotateLerp);
+            transform.eulerAngles = Vector3.forward * AngleValue;
+            m_bottleMaterial.SetFloat("_ScaleAndRotate", m_curveScaleAndRotation.Evaluate(AngleValue));
+            //
+            m_timeRotateCurrent += Time.deltaTime;
+            //
+            yield return new WaitForEndOfFrame();
+        }
+        //
+        AngleValue = 0f;
+        transform.eulerAngles = Vector3.forward * AngleValue;
+        m_bottleMaterial.SetFloat("_ScaleAndRotate", m_curveScaleAndRotation.Evaluate(AngleValue));
+    }
+}
+
+#if UNITY_EDITOR
+
+[CustomEditor(typeof(BottleController))]
+public class BottleControllerEditor : Editor
+{
+    private BottleController m_target;
+
+    private SerializedProperty m_timeRotate;
+    //
+    private SerializedProperty m_curveFillAmount;
+    private SerializedProperty m_curveScaleAndRotation;
+    private SerializedProperty m_curveData;
+
+    private void OnEnable()
+    {
+        m_target = target as BottleController;
+        //
+        m_timeRotate = QEditorCustom.GetField(this, "m_timeRotate");
+        //
+        m_curveFillAmount = QEditorCustom.GetField(this, "m_curveFillAmount");
+        m_curveScaleAndRotation = QEditorCustom.GetField(this, "m_curveScaleAndRotation");
+        m_curveData = QEditorCustom.GetField(this, "m_curveData");
+    }
+
+    //public override void OnInspectorGUI()
+    //{
+    //    QEditorCustom.SetUpdate(this);
+    //    //
+    //    QEditorCustom.SetLabel("SETTING", QEditorCustom.GetGUILabel(FontStyle.Bold, TextAnchor.MiddleCenter));
+    //    QEditorCustom.SetField(m_timeRotate);
+    //    //
+    //    QEditorCustom.SetSpace(10);
+    //    //
+    //    QEditorCustom.SetLabel("COLOR", QEditorCustom.GetGUILabel(FontStyle.Bold, TextAnchor.MiddleCenter));
+    //    for (int i = 0; i < m_target.BottleColor.Count; i++)
+    //    {
+    //        QEditorCustom.SetHorizontalBegin();
+    //        QEditorCustom.SetLabel(i.ToString(), QEditorCustom.GetGUILabel(FontStyle.Normal, TextAnchor.MiddleCenter), QEditorCustom.GetGUIWidth(25));
+    //        m_target.BottleColor[i] = QEditorCustom.SetField(m_target.BottleColor[i], QEditorCustom.GetGUIWidth(100));
+    //        QEditorCustom.SetHorizontalEnd();
+    //    }
+    //    //
+    //    QEditorCustom.SetSpace(10);
+    //    //
+    //    QEditorCustom.SetLabel("ANIMATION", QEditorCustom.GetGUILabel(FontStyle.Bold, TextAnchor.MiddleCenter));
+    //    QEditorCustom.SetField(m_curveFillAmount);
+    //    QEditorCustom.SetField(m_curveScaleAndRotation);
+    //    QEditorCustom.SetField(m_curveData);
+    //    //
+    //    QEditorCustom.SetApply(this);
+    //}
+}
+
+#endif
