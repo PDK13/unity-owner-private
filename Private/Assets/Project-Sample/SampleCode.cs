a88f919975214d1078206bff4d9ae58df3701e66
PDK13 <khoa199960@gmail.com>
2024-03-01 12:04:06 +0700

    update

--- Private/Assets/Project-Sample/SampleCode.cs
+++ Private/Assets/Project-Sample/SampleCode.cs
@@ -7,28 +7,36 @@ public class SampleCode : MonoBehaviour
     private int value = 3;
     private int? number = null;
 
+    private List<int> numbers;
+
     private void Awake()
     {
-        //Debug.Log("[number] == null = " + (number == null));
-        //Debug.Log("[number] is null = " + (number is null));
-        //Debug.Log("[number] ?? 5 = " + (number ?? 5));
-        //Debug.Log("-------------------------------------");
-        ////
-        //number = (number ??= value);
-        //Debug.Log("[number] ??= value (value = " + value + ")");
-        //Debug.Log("-------------------------------------");
-        ////
-        //Debug.Log("[number] == null = " + (number == null));
-        //Debug.Log("[number] is null = " + (number is null));
-        //Debug.Log("[number] ?? 5 = " + (number ?? 5));
-        //Debug.Log("-------------------------------------");
-        ////
-        //Debug.Log("[number] value = " + number);
+        
     }
 
-    private void Start()
+    private void Test01()
     {
-        //QPlayerPrefs.SetValue(null, 3);
-        Debug.Log("[pref] " + QPlayerPrefs.GetValueInt(""));
+        Debug.Log("[number] == null = " + (number == null));
+        Debug.Log("[number] is null = " + (number is null));
+        Debug.Log("[number] ?? 5 = " + (number ?? 5));
+        Debug.Log("-------------------------------------");
+        //
+        number = (number ??= value);
+        Debug.Log("[number] ??= value (value = " + value + ")");
+        Debug.Log("-------------------------------------");
+        //
+        Debug.Log("[number] == null = " + (number == null));
+        Debug.Log("[number] is null = " + (number is null));
+        Debug.Log("[number] ?? 5 = " + (number ?? 5));
+        Debug.Log("-------------------------------------");
+        //
+        Debug.Log("[number] value = " + number);
+    }
+
+    private void Test02()
+    {
+        Debug.Log("[numbers] count " + (numbers != null ? numbers.Count : "null"));
+        (numbers ??= new List<int>()).Add(value);
+        Debug.Log("[numbers] count " + (numbers != null ? numbers.Count : "null"));
     }
 }
